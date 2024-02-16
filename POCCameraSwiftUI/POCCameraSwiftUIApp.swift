182c478ac70f4e53da0e3058d38a2ee34777577b
Leonardo Medeiros <leohsmedeiros@gmail.com>
2024-02-16 14:15:18 -0300

    Initial Commit

---
+++ POCCameraSwiftUI/POCCameraSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  POCCameraSwiftUIApp.swift
+//  POCCameraSwiftUI
+//
+//  Created by Leonardo Medeiros on 16/02/24.
+//
+
+import SwiftUI
+
+@main
+struct POCCameraSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
